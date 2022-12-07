ec8902acd1fab7d525486f7a4d87104a85719270
ppotatoG <thgus2270@gmail.com>
2022-12-07 17:37:29 +0900

    부모 컴포넌트로 데이터 전달하기

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,9 +6,7 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
-  constructor() {}
-
-  ngOnInit() {}
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
